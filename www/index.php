fecfe2dbc0017b60cb96863fc3ea657999931234
Tomáš Grundza <tomas@grundza.cz>
2024-11-02 19:30:59 +0100

    🚀 Create index.php

---
+++ www/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$error = 0;
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Diplomka.Grundza.cz</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+</head>
+<body>
+
+
+<article>
+    <h1>Diplomka.Grundza.cz</h1>
+    <div>
+        <p>Toto je výchozí stránka aplikace. Pokračujte validním voláním API endpointu.</p>
+        
+    </div>
+</article>
+</body>
+</html>
